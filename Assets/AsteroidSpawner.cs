5d77578193d462e8ee5412a871205eae2d12493c
Evan Fletcher <evan.fletcher42@gmail.com>
2016-06-30 00:20:05 -0600

    Add AsteroidSpawner.  Fills the scene with a bunch of asteroids on init.

---
+++ Assets/AsteroidSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidSpawner : MonoBehaviour {
+
+    private static GameObject prefabAsteroid;
+
+
+    // Use this for initialization
+    void Start () {
+        prefabAsteroid = Resources.Load("Asteroid") as GameObject;
+        for (int i = 0; i < 30; i++)
+        {
+            GameObject asteroid = Instantiate(prefabAsteroid, Random.insideUnitSphere * 1.0f + new Vector3(0, 2.0f, 0), Quaternion.identity) as GameObject;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
